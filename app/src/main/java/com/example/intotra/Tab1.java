636955aca1e0bc8888c695d5c1fc06da6040e64e
logesh <logeshwaran.sr1@gmail.com>
2019-10-12 00:40:47 +0530

    commit_intotra1.2

--- app/src/main/java/com/example/intotra/Tab1.java
+++ app/src/main/java/com/example/intotra/Tab1.java
@@ -34,15 +34,7 @@ public class Tab1 extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Tab1.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static Tab1 newInstance(String param1, String param2) {
         Tab1 fragment = new Tab1();
         Bundle args = new Bundle();
@@ -62,8 +54,8 @@ public class Tab1 extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_tab1, container, false);
     }
@@ -90,16 +82,7 @@ public class Tab1 extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
+
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
